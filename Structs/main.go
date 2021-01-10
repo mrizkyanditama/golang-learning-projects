31224b4387ada92e6ec1932a6108a2a36ccf9289
Muhammad Rizky Anditama <rizkind@gmail.com>
2021-01-10 17:57:23 +0700

    learn structs

---
+++ Structs/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contact   contactInfo
+}
+
+func main() {
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contact: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+	jim.updateName("jimmy")
+	jim.print()
+}
+
+func (personPointer *person) updateName(newFirstname string) {
+	(*personPointer).firstName = newFirstname
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
